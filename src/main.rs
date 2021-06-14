23c18cd8204024cfdf3220216dda8c30612c9fca
mryhryki <mryhryki@gmail.com>
2021-06-15 08:35:52 +0900

    Root endpoint reponds list S3 objects.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,34 @@
 use actix_web::{get, web, App, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct S3Object {
+    pub key: String,
+}
 
 #[get("/")]
-async fn index(web::Path(()): web::Path<()>) -> impl Responder {
-    format!("Hello Actix-Web!")
+async fn index(_path: web::Path<()>) -> impl Responder {
+    let client = s3::Client::from_env();
+    let req = client.list_objects_v2().bucket("mryhryki-temp");
+    let resp = req.send().await;
+
+    let mut contents: Vec<S3Object> = vec![];
+    match resp {
+        Ok(val) => match val.contents {
+            Some(val) => {
+                for object in val {
+                    contents.push(S3Object {
+                        key: object.key.unwrap_or(String::from("(unknown)")),
+                    })
+                }
+            }
+            None => (),
+        },
+        Err(_) => (),
+    }
+
+    let body: String = serde_json::to_string_pretty(&contents).unwrap();
+    format!("{}", body)
 }
 
 #[actix_web::main]
